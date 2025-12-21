ea15f6d86136398629cbe6261d4bff06df6ecb23
Butters7 <egorka.davydov.1980@mail.ru>
2025-12-21 19:36:42 +0300

    feat(helloap) Добавил flask и requests и изменил логику работы

--- labs/lab05/helloapp/hello.py
+++ labs/lab05/helloapp/hello.py
@@ -5,8 +5,17 @@ Author: Butters7
 """
 
 import typer
+import requests
+from flask import Flask
 
 APP_NAME: str = "Hello appsec world"
+app = Flask(__name__)
+
+
+@app.route("/")
+def index() -> str:
+    """Return greeting message."""
+    return f"{APP_NAME}!"
 
 
 def main(name: str = typer.Option(..., prompt="Enter your name")) -> None:
@@ -17,6 +26,8 @@ def main(name: str = typer.Option(..., prompt="Enter your name")) -> None:
         name: The name of the user to greet.
     """
     print(f"{APP_NAME} from @{name}")
+    print(f"Flask version: {Flask.__name__}")
+    print(f"Requests version: {requests.__version__}")
 
 
 if __name__ == "__main__":
